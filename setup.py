12faafcbe52b23a1a53c5a7dfdcda7bb5e4d464e
rohitkr8527 <rohitkr7518@gmail.com>
2025-08-25 04:07:16 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Rohit Kumar",
+    author_email="rohitkr7518@gmail.com",
+    packages=find_packages(),
+)
